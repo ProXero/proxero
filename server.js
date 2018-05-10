685ee423e8cbcb642c0f386372ddda4c5796fe1d
Csaba Breitenstein <csaba.breitenstein@agcocorp.com>
2018-05-10 14:08:34 +0200

    fix?

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/*', function(req, res) {
 // Run the app by serving the static files
 // in the dist directory
 
-app.use(express.static(__dirname + '/dist'));
+//app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default
 // Heroku port
